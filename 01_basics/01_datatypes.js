bd5b61805b7e5db97bdaa0f2a8a29183f96a400e
pallavibendigeri <pallavibendigeri3@gmail.com>
2024-12-08 20:47:48 +0530

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treate all js code as newer version
+
+alert("Hello") //it will not run(throw an error) bcz we are using node.js not browser
+
+console.log(2+3); //code readability should be high
+
+let name ="pallavi";
+let age = 21
+let isLoggedIn=false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null = standalone value //ex: temp = null;
+//undefined => not defined
+// symbol => unique
+
+
+//object
+
+console.log(typeof "pallavi") // string]
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 122345;
+let accountEmail = "pallavi@gmail.com";
+var accountPassword = "12345";
+accountCity = "belagavi";
+let accountState;
+
+// accountId=2  //not allowed
+
+accountEmail="pmb@gmail.com";
+accountPassword="765443";
+accountCity="bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
