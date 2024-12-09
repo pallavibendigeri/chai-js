8d85ad8c9d7c759587b41c9865c2a4fb0e34624d
pallavibendigeri <pallavibendigeri3@gmail.com>
2024-12-09 21:31:49 +0530

    1

--- 01_basics/01_ConversionOperations.js
+++ 01_basics/01_ConversionOperations.js
@@ -1,19 +1,59 @@
-let score = "3abc"
+// let score = "3abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber) //NaN for "3abc" and undefined and string  // 0 for null and false //1 for true //number of 244
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN for "3abc" and undefined and string  // 0 for null and false //1 for true //number of 244
 
-let isLoggedIn = 1 //1=true // 0=false //"pallavi=true" //empty=false
-let booleanIsloggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1 //1=true // 0=false //"pallavi=true" //empty=false
+// let booleanIsloggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
 
 
-someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber) //converted to string
+// someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber) //converted to string
+
+// ********************************* Operations ***********************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Pallavi"
+let str3 = str1+str2;
+console.log(str3)
+
+console.log("1"+"2");
+console.log(1+"2" + 2);
+console.log(1+2+"2");
+
+console.log(+true);  //1
+console.log(true); //true
+console.log(+""); //0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter); //101
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //101
+
