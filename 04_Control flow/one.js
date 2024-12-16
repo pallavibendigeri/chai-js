196ca761369ecf993246fe2336087bd1d6151d20
pallavibendigeri <pallavibendigeri3@gmail.com>
2024-12-16 21:42:49 +0530

    flow in js

---
+++ 04_Control flow/one.js
@@ -0,0 +1,25 @@
+// if - statement
+const IsUserLoggedIn = true
+const temparature = 41
+if(temparature<50){
+    console.log("Less than 50");
+} else{
+console.log("Temparature is grater than 50");
+}
+
+const balance = 1000
+//if(balance > 500) console.log("test"),console.log("test2");
+
+//nested if-else
+
+if(balance < 500){
+    console.log('1');    
+}
+else if(balance < 750){
+    console.log("2");
+}else{
+    console.log("3////");
+    
+}
+
+
