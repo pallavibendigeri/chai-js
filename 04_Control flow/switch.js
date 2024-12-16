c2080a0f23f3a2db49479fc4123e0b244170bef4
pallavibendigeri <pallavibendigeri3@gmail.com>
2024-12-16 21:43:05 +0530

    switch statement in js

---
+++ 04_Control flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("invalid");
+        
+        break;
+}
